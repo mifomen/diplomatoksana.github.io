14c802f4c0ca72e8a1986aa8c68e6fb9f159f2b4
Ilya Koshelev <mifomen@yandex.ru>
2018-08-22 13:01:51 +0300

    switch to gulp v4

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,21 @@
-require('require-dir')('./gulp', {recurse: true})
+'use strict'
+
+
+var gulp = require('gulp'),
+       $ = require('gulp-load-plugins')();
+
+gulp.task('pug',function(){
+return gulp.src('src/pug/*.pug')
+.pipe($.pug({
+  pretty:true
+}))
+.pipe(gulp.dest('build'))
+});
+
+  gulp.task('scss',function () {
+    return gulp.src('src/scss/main.scss')
+    .pipe($.sass()).on('error', $.sass.logError)
+    .pipe($.csso())
+    .pipe(gulp.dest('build/css'));
+
+  });
