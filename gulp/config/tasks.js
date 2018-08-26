a44e3535280a312283daacc7fcf45cbe14bd2729
Ilya Koshelev <mifomen@yandex.ru>
2018-08-26 23:59:16 +0300

    delete .js

--- gulp/config/tasks.js
+++ gulp/config/tasks.js
@@ -1,10 +1,10 @@
 module.exports = [
-'./gulp/tasks/pug.js',
-'./gulp/tasks/scss.js',
-'./gulp/tasks/watch.js',
-'./gulp/tasks/img.js',
-'./gulp/tasks/compress-js.js',
-'./gulp/tasks/clear.js',
-'./gulp/tasks/serve.js',
-'./gulp/tasks/build.js',
+'./gulp/tasks/pug',
+'./gulp/tasks/scss',
+'./gulp/tasks/watch',
+'./gulp/tasks/img',
+'./gulp/tasks/compress-js',
+'./gulp/tasks/clear',
+'./gulp/tasks/serve',
+'./gulp/tasks/build',
 ];
