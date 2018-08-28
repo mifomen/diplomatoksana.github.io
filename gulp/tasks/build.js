acfe4a4b7a09d3e8c252c9a19ae18b8da8a80457
mifomen <mifomen@yandex.ru>
2018-08-28 14:10:40 +0300

    ..

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,15 +2,17 @@ module.exports = function () {
   $.gulp.task('build', 
     $.gulp.series(
       'clear',
-      'html2pug',
+      // 'html2pug',
       'pug',
       $.gulp.parallel('img', 'scss'),
-      $.gulp.parallel('scripts:lib','script')
-    // 'serve'
+      $.gulp.parallel('scripts:lib','script'),
+    'serve',
+    'watch'
     ));
   $.gulp.task('default', 
     $.gulp.series(
       'build',
-      'serve'
+      'serve',
+      'watch'
       ));
 }
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,8 @@
 module.exports = function () {
   $.gulp.task('watch', function() {
-    $.gulp.watch('src/scss/**/*.scss', gulp.parallel('scss'))
-    $.gulp.watch('src/pug/main.pug', gulp.parallel('pug'))
-    $.gulp.watch('src/js/**/*.js', gulp.parallel('compress-js'))
+    $.gulp.watch("src/scss/**/*.scss",  $.gulp.parallel('scss'));
+    $.gulp.watch("src/pug/**/*.pug",  $.gulp.parallel('pug'));
+    $.gulp.watch("src/js/**/*.js",  $.gulp.parallel('script'));
+    $.gulp.watch("src/pug/html2pug/*.html",  $.gulp.series('html2pug','pug'));
   });
 }
